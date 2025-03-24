0d78b79c5321737956852d642233ae64863d8483
Виктория <pestovav68@gmail.com>
2025-03-24 16:42:32 +0500

    Добавлен HtmlWebpackPlugin для генерации HTML-файла и обновлены зависимости в package.json и package-lock.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
@@ -5,17 +6,25 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    clean: true,   
+    filename: 'bundle.[contenthash].js',
+    clean: true,
   },
-  devtool: 'source-map',  
+  devtool: 'source-map',
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: 'public' },
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
       ],
     }),
-  ],
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+  ], 
   module: {
     rules: [
       {
@@ -29,5 +38,5 @@ module.exports = {
         },
       },
     ]
-  }
-};
+  },
+}; 
