d017652209712181b03fdab42d71f4b20510959f
Виктория <pestovav68@gmail.com>
2025-03-24 16:25:47 +0500

    Добавлены зависимости Babel и настроен webpack для использования babel-loader с preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,18 @@ module.exports = {
       ],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ]
+  }
 };
